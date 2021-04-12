a0f54d5187dad1b0dadfc466e2f0e075b278b3c1
TJ James <jamestw13@gmail.com>
2021-04-12 00:25:49 -0500

    Put pieces on board

--- src/com/hnef/gui/Table.java
+++ src/com/hnef/gui/Table.java
@@ -1,18 +1,24 @@
 package com.hnef.gui;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
-import javax.swing.border.StrokeBorder;
 
+import com.hnef.engine.board.Board;
 import com.hnef.engine.board.BoardUtils;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.awt.GridBagLayout;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -22,10 +28,12 @@ import java.util.List;
 public class Table {
   private final JFrame gameFrame;
   private final BoardPanel boardPanel;
+  private final Board hnefBoard;
 
   private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
   private final static Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 530);
   private final static Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
+  private static String defaultPieceImagesPath = "art/";
 
   public Table() {
     this.gameFrame = new JFrame("JHnef");
@@ -33,6 +41,7 @@ public class Table {
     final JMenuBar tableMenuBar = createTableMenuBar();
     this.gameFrame.setJMenuBar(tableMenuBar);
     this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
+    this.hnefBoard = Board.createHnefataflBoard();
     this.boardPanel = new BoardPanel();
     this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
     this.gameFrame.setVisible(true);
@@ -88,8 +97,22 @@ public class Table {
       setPreferredSize(TILE_PANEL_DIMENSION);
       setBackground(Color.decode("#593E1A"));
       setBorder(border);
-      System.out.println("check");
+      assignTilePieceIcon(hnefBoard);
       validate();
+    }
+
+    private void assignTilePieceIcon(final Board board) {
+      this.removeAll();
+      if (board.getTile(this.tileId).isTileOccupied()) {
+        try {
+          final BufferedImage image = ImageIO.read(new File(defaultPieceImagesPath
+              + board.getTile(this.tileId).getPiece().getPieceAlliance().toString().substring(0, 1)
+              + board.getTile(this.tileId).getPiece().toString() + ".gif"));
+          add(new JLabel(new ImageIcon(image)));
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
     }
 
   }
